c472c6c1f60c939f2ce9a5e1f3e74a4a8129c6d3
deniz <uysaldeniz200@gmail.com>
2023-03-05 19:43:04 +0300

    update v.1.01 some errors fixed. some src not downloadable!

--- manageclasses.py
+++ manageclasses.py
@@ -2,24 +2,19 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 import os
-
-if not os.path.exists('downloaded_files'):
-    os.makedirs('downloaded_files')
+if not os.path.exists('my_downloaded_files'):
+    os.makedirs('my_downloaded_files')
 
 # Sayfa URL'sini ayarlayın
 url = 'url'
 
-# GET isteği gönderin ve sayfanın kaynak kodunu indirin
+# GET isteği gönderin ve sayfanın kaynak kodunu indirin.
 response = requests.get(url)
-
-
 soup = BeautifulSoup(response.text, 'html.parser')
+#foundclasses.py kullanıp silmek istediğiniz class"ı giriniz.
 
-#class yazın
-
-for element in soup.find_all(class_="class"):
+for element in soup.find_all(class_="None"):
     element.extract()
-
 
 for tag in soup.find_all():
     if tag.has_attr('href') and tag['href'] != 'javascript:void(0)':
@@ -29,12 +24,14 @@ for tag in soup.find_all():
     else:
         continue
 
-    filename = os.path.join('downloaded_files', os.path.basename(file_url))
+    filename = os.path.join('my_downloaded_files', os.path.basename(file_url))
+    if not filename.endswith('.html'):
+        filename += '.html'
 
     file_response = requests.get(file_url)
     with open(filename, 'wb') as f:
         f.write(file_response.content)
 
-filename = os.path.join('downloaded_files', 'index.html')
+filename = os.path.join('my_downloaded_files', 'index.html')
 with open(filename, 'w') as f:
     f.write(str(soup))
